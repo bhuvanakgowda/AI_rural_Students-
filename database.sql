f4e153f2b6e4e5307552bf8522316f7d5d5e8aba
Bhuvaneshwari K <bhuvanak2411@gmail.com>
2026-07-17 14:44:44 +0530

    Add progress table to database schema

--- database.sql
+++ database.sql
@@ -13,3 +13,14 @@ email VARCHAR(100),
 password VARCHAR(100)
 
 );
+CREATE TABLE progress(
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+
+student VARCHAR(100),
+
+quiz_score INT,
+
+completed_date DATE
+
+);
