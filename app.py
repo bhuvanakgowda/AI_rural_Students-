5cfe21d16e8ccb2e9af96828e348e8ce6613fbbd
Bhuvaneshwari K <bhuvanak2411@gmail.com>
2026-07-17 14:31:14 +0530

    Initialize Flask app with user registration and login

---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import mysql.connector
+
+app = Flask(__name__)
+app.secret_key = "eduassist"
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="eduassist"
+)
+
+cursor = db.cursor()
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    if request.method == "POST":
+
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        sql = "INSERT INTO users(name,email,password) VALUES(%s,%s,%s)"
+        values = (name, email, password)
+
+        cursor.execute(sql, values)
+        db.commit()
+
+        return redirect('/login')
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == "POST":
+
+        email = request.form['email']
+        password = request.form['password']
+
+        sql = "SELECT * FROM users WHERE email=%s AND password=%s"
+        values = (email, password)
+
+        cursor.execute(sql, values)
+
+        user = cursor.fetchone()
+
+        if user:
+            session['user'] = user[1]
+            return redirect('/dashboard')
+        else:
+            return "Invalid Login"
+
+    return render_template('login.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+
+    if 'user' in session:
+        return render_template('dashboard.html', name=session['user'])
+
+    return redirect('/login')
+
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
+
+
+@app.route('/logout')
+def logout():
+
+    session.pop('user', None)
+
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
