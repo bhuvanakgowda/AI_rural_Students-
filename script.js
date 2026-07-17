08f27966bb06dc5ec6818d95585059b8245114c5
Bhuvaneshwari K <bhuvanak2411@gmail.com>
2026-07-17 14:42:14 +0530

    Implement async question sending and response display
    Added an async function to send questions and display AI responses.

--- script.js
+++ script.js
@@ -5,3 +5,44 @@ alert("Welcome to EduAssist AI!");
 }
 
 console.log("EduAssist AI Loaded");
+async function sendQuestion(){
+
+let question=document.getElementById("question").value;
+
+let form=new FormData();
+
+form.append("question",question);
+
+let response=await fetch("/ask",{
+
+method:"POST",
+
+body:form
+
+});
+
+let data=await response.json();
+
+document.getElementById("chatBox").innerHTML+=`
+
+<p>
+
+<b>You:</b> ${question}
+
+</p>
+
+<p>
+
+<b>EduAssist AI:</b>
+
+${data.answer}
+
+</p>
+
+<hr>
+
+`;
+
+document.getElementById("question").value="";
+
+}
