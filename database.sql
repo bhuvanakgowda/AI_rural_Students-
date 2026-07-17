952b63ef0866e6d4c4ce75e2fbbc531196453ac6
Bhuvaneshwari K <bhuvanak2411@gmail.com>
2026-07-17 14:32:41 +0530

    Create eduassist database and users table

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE eduassist;
+
+USE eduassist;
+
+CREATE TABLE users(
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+
+name VARCHAR(100),
+
+email VARCHAR(100),
+
+password VARCHAR(100)
+
+);
